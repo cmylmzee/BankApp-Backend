29cddff162af884ebe4e0ee3ae2b9901bd84f5e9
Cem Yılmaz <yilmazcem1122@gmail.com>
2024-10-04 14:57:10 +0300

    Packages are created, entity is created

---
+++ src/main/java/com/example/bankapp/entity/Account.java
@@ -0,0 +1,26 @@
+package com.example.bankapp.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "accounts") // TABLE NAME İS ACCOUNTS
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private double balance;
+}
